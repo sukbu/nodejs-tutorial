597315d4f1fc147fe616402551202f54b82bd10e
sukbu <tjrqja12345@gmail.com>
2019-10-13 14:46:22 +0900

    syntax: loop, array nodejs: readdir(path, callback)

---
+++ syntax/array.js
@@ -0,0 +1,10 @@
+var arr = ['A','B','C','D'];
+
+console.log(arr[0]);
+arr[1]=2;
+
+console.log(arr);
+console.log(arr.lastIndexOf('C'));
+console.log(arr.length);
+arr.push('E');
+console.log(arr);
---
+++ syntax/loop.js
@@ -0,0 +1,13 @@
+console.log('a');
+console.log('b');
+
+for(var i=0; i<10; i++){
+    console.log('c1');
+}
+console.log(i);
+var i=0;
+while(i<10){
+    console.log('c2');
+    i=i+1;
+}
+console.log('d');
