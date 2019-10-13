2c97f925a11243173411c6aaaac3827b7dca584b
sukbu <tjrqja12345@gmail.com>
2019-10-13 16:52:48 +0900

    Receive data with post method

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
+
+function templateHTML(title, list, body) {
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB2</a></h1>
+    ${list}
+    <a href="/create">create</a>
+    <h2>${title}</h2>
+    <p>${body}</p>
+  </body>
+  </html>
+`;
+}
+
+
+function templateList(files) {
+  var list = '<ul>';
+  var i = 0;
+  while (i < files.length) {
+    list = list + `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`;
+    i = i + 1;
+  }
+  list = list + '</ul>';
+
+  return list;
+}
+
 
 var server = http.createServer(function (request, response) {
   var _url = request.url;
@@ -12,65 +46,61 @@ var server = http.createServer(function (request, response) {
       fs.readdir('./data', function (error, files) {
         var title = 'Welcome';
         var description = 'Hello Node.js';
-        var list = '<ul>';
-        var i = 0;
-        while (i < files.length) {
-          list = list + `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`;
-          i = i + 1;
-        }
-        list = list + '</ul>';
-        var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-      `;
+        var list = templateList(files);
+        var template = templateHTML(title, list, `${description}`);
         response.writeHead(200);
         response.end(template);
       })
     } else {
       fs.readdir('./data', function (error, files) {
-        var title = 'Welcome';
-        var description = 'Hello Node.js';
-        var list = '<ul>';
-        var i = 0;
-        while (i < files.length) {
-          list = list + `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`;
-          i = i + 1;
-        }
-        list = list + '</ul>';
         fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+          var list = templateList(files);
           var title = queryData.id;
-          var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB - ${title}</a></h1>
-          ${list}
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-    `;
+          var template = templateHTML(title, list, `${description}`);
           response.writeHead(200);
           response.end(template);
         });
       });
     }
-  } else {
+  } else if (pathname === '/create') {
+    fs.readdir('./data', function (error, files) {
+      var title = 'Web - create';
+      var list = templateList(files);
+      var template = templateHTML(title, list, 
+        `
+        <form action="http://localhost:3000/create_process" method="POST">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+            <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+            <input type="submit">
+        </p>
+        </form>
+        `);
+      response.writeHead(200);
+      response.end(template);
+    })
+  } else if(pathname === '/create_process'){
+    var body = '';
+    request.on('data',function(data){
+      body += data;
+
+      if(body.length > 1e6){
+        request.connection.destroy();
+      }
+    });
+    request.on('end',function(){
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+
+      //console.log(post.title);
+    });
+    response.writeHead(200);
+    response.end('success');
+  }
+  else {
     response.writeHead(404);
     response.end('404 Not found');
   }
