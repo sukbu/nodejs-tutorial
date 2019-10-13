e077f996d0aa8c8f01af85df39123294575384aa
sukbu <tjrqja12345@gmail.com>
2019-10-13 17:25:07 +0900

    Update method

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-function templateHTML(title, list, body) {
+function templateHTML(title, list, body, control) {
   return `
   <!doctype html>
   <html>
@@ -14,8 +14,7 @@ function templateHTML(title, list, body) {
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
-    <a href="/create">create</a>
-    <h2>${title}</h2>
+    ${control}
     <p>${body}</p>
   </body>
   </html>
@@ -47,7 +46,9 @@ var server = http.createServer(function (request, response) {
         var title = 'Welcome';
         var description = 'Hello Node.js';
         var list = templateList(files);
-        var template = templateHTML(title, list, `${description}`);
+        var template = templateHTML(title, list, 
+          `<h2>${title}</h2>${description}`,
+          `<a href="/create">create</a>`);
         response.writeHead(200);
         response.end(template);
       })
@@ -56,7 +57,9 @@ var server = http.createServer(function (request, response) {
         fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
           var list = templateList(files);
           var title = queryData.id;
-          var template = templateHTML(title, list, `${description}`);
+          var template = templateHTML(title, list, 
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
           response.writeHead(200);
           response.end(template);
         });
@@ -77,7 +80,7 @@ var server = http.createServer(function (request, response) {
             <input type="submit">
         </p>
         </form>
-        `);
+        `, '');
       response.writeHead(200);
       response.end(template);
     })
