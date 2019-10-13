b900284f4722f82fed36a4111ffb0f0ca336eb5d
sukbu <tjrqja12345@gmail.com>
2019-10-13 18:04:42 +0900

    Delete method

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ var server = http.createServer(function (request, response) {
         var title = 'Welcome';
         var description = 'Hello Node.js';
         var list = templateList(files);
-        var template = templateHTML(title, list, 
+        var template = templateHTML(title, list,
           `<h2>${title}</h2>${description}`,
           `<a href="/create">create</a>`);
         response.writeHead(200);
@@ -57,9 +57,15 @@ var server = http.createServer(function (request, response) {
         fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
           var list = templateList(files);
           var title = queryData.id;
-          var template = templateHTML(title, list, 
+          var template = templateHTML(title, list,
             `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+            ` <a href="/create">create</a> 
+              <a href="/update?id=${title}">update</a>
+              <form action="delete_process" method="POST">
+                <input type="hidden" name="id" value="${title}">
+                <input type="submit" value="delete">
+              </form>`
+          );
           response.writeHead(200);
           response.end(template);
         });
@@ -102,18 +108,18 @@ var server = http.createServer(function (request, response) {
         if (err) throw err;
         console.log()
 
-        response.writeHead(302, {Location: `/?id=${title}`});
+        response.writeHead(302, { Location: `/?id=${title}` });
         response.end();
-      });
+      })
 
       //console.log(post.title);
     });
-  } else if(pathname === '/update'){
+  } else if (pathname === '/update') {
     fs.readdir('./data', function (error, files) {
       fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
         var list = templateList(files);
         var title = queryData.id;
-        var template = templateHTML(title, list, 
+        var template = templateHTML(title, list,
           `
           <form action="/update_process" method="POST">
           <input type="hidden" name="id" value="${title}">
@@ -122,14 +128,57 @@ var server = http.createServer(function (request, response) {
               <textarea name="description" placeholder="description" >${description}</textarea>
           </p>
           <p>
-              <input type="submit" >
+              <input type="submit">
           </p>
           </form>
           `,
-          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+        );
         response.writeHead(200);
         response.end(template);
       });
+    });
+  } else if (pathname === '/update_process') {
+    var body = '';
+    request.on('data', function (data) {
+      body += data;
+
+      if (body.length > 1e6) {
+        request.connection.destroy();
+      }
+    });
+    request.on('end', function () {
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
+      fs.rename(`data/${id}`, `data/${title}`, function (err) {
+        fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+          if (err) throw err;
+          response.writeHead(302, { Location: `/?id=${title}` });
+          response.end();
+        })
+        if (err) {
+          console.log('error: ' + err);
+          throw err;
+        }
+      })
+    });
+  } else if (pathname === '/delete_process') {
+    var body = '';
+    request.on('data', function (data) {
+      body += data;
+      if (body.length > 1e6) {
+        request.connection.destroy();
+      }
+    });
+    request.on('end', function () {
+      var post = qs.parse(body);
+      var id = post.id;
+      fs.unlink(`data/${id}`, function (err) {
+        response.writeHead(302, { Location: `/` });
+        response.end();
+      })
     });
   } else {
     response.writeHead(404);
