869fa64c48c4414fabe6487b3f925a3e88dfcae9
sukbu <tjrqja12345@gmail.com>
2019-10-13 15:20:05 +0900

    nodejs: data auto change javascript syntax: function

--- main.js
+++ main.js
@@ -5,39 +5,52 @@ var url = require('url');
 var server = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
-  var pathname = url.parse(_url,true).pathname;
-   
-  if(pathname=='/'){
-    if(queryData.id === undefined){
-      fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+  var pathname = url.parse(_url, true).pathname;
+
+  if (pathname == '/') {
+    if (queryData.id === undefined) {
+      fs.readdir('./data', function (error, files) {
         var title = 'Welcome';
         var description = 'Hello Node.js';
+        var list = '<ul>';
+        var i = 0;
+        while (i < files.length) {
+          list = list + `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`;
+          i = i + 1;
+        }
+        list = list + '</ul>';
         var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-    `;
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+      `;
         response.writeHead(200);
         response.end(template);
-      });
+      })
     } else {
-      fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
-        var title = queryData.id;
-        var template = `
+      fs.readdir('./data', function (error, files) {
+        var title = 'Welcome';
+        var description = 'Hello Node.js';
+        var list = '<ul>';
+        var i = 0;
+        while (i < files.length) {
+          list = list + `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`;
+          i = i + 1;
+        }
+        list = list + '</ul>';
+        fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+          var title = queryData.id;
+          var template = `
         <!doctype html>
         <html>
         <head>
@@ -45,19 +58,16 @@ var server = http.createServer(function (request, response) {
           <meta charset="utf-8">
         </head>
         <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
+          <h1><a href="/">WEB - ${title}</a></h1>
+          ${list}
           <h2>${title}</h2>
           <p>${description}</p>
         </body>
         </html>
     `;
-        response.writeHead(200);
-        response.end(template);
+          response.writeHead(200);
+          response.end(template);
+        });
       });
     }
   } else {
---
+++ syntax/function.js
@@ -0,0 +1,13 @@
+f123();
+
+function f123(){
+    console.log(1);
+    console.log(2);
+    console.log(3);
+}
+
+function sum(first,second){ // params
+    console.log(first+second);
+}
+
+sum(2,4); // args
